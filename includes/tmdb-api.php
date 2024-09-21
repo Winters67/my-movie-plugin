c12b8ed2365ed973415d764acc24753b5359a8eb
Winters67 <emmanuel.vbg@gmail.com>
2024-09-21 21:34:34 +0200

    mise en cache des réponses de l'API

--- includes/tmdb-api.php
+++ includes/tmdb-api.php
@@ -2,7 +2,16 @@
 // Fonction pour récupérer les films tendances depuis TMDb
 function get_trending_movies($time_window = 'day')
 {
-    $bearer_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo'; 
+    // Clé de cache pour stocker les films tendances
+    $cache_key = 'trending_movies_' . $time_window;
+
+    // Vérifier s'il existe déjà une version en cache
+    $cached_movies = get_transient($cache_key);
+    if ($cached_movies) {
+        return $cached_movies;
+    }
+
+    $bearer_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo';
 
     $url = 'https://api.themoviedb.org/3/trending/movie/' . $time_window;
 
@@ -21,6 +30,43 @@ function get_trending_movies($time_window = 'day')
 
     $body = wp_remote_retrieve_body($response);
     $data = json_decode($body, true);
+    $movies = $data['results'] ?? [];
 
-    return $data['results'] ?? [];
+    // Maintenant, récupérons les détails pour chaque film
+    $detailed_movies = [];
+    foreach ($movies as $movie) {
+        $movie_details = get_movie_details($movie['id'], $bearer_token);
+        if ($movie_details) {
+            $detailed_movies[] = $movie_details;
+        }
+    }
+
+    // Mettre les résultats dans le cache pour 6 heures (21600 secondes)
+    set_transient($cache_key, $detailed_movies, 6 * HOUR_IN_SECONDS);
+
+    return $detailed_movies;
+}
+
+// Fonction pour récupérer les détails d'un film spécifique depuis TMDb
+function get_movie_details($movie_id, $bearer_token)
+{
+    $url = 'https://api.themoviedb.org/3/movie/' . $movie_id;
+
+    sleep(1); // Pause d'une seconde pour respecter la limitation d'API
+
+    $response = wp_remote_get($url, array(
+        'headers' => array(
+            'Authorization' => 'Bearer ' . $bearer_token,
+        ),
+    ));
+
+    if (is_wp_error($response)) {
+        error_log('Erreur lors de la récupération des détails du film ID : ' . $movie_id . ' - ' . $response->get_error_message());
+        return null;
+    }
+
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
+    return $data ?? null;
 }
